be5f98bf087e04ef847542db6a5b51589af14b20
Neha Joshi <nehajoshimjnj@gmail.com>
2026-03-26 00:39:02 +0530

    Add Binance client wrapper with dotenv support

---
+++ bot/client.py
@@ -0,0 +1,10 @@
+from binance.client import Client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("BINANCE_API_KEY")
+API_SECRET = os.getenv("BINANCE_API_SECRET")
+
+client = Client(API_KEY, API_SECRET, testnet=True)
